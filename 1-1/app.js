9bd8eaa91e6d8d2ec441d5b0b3e8edb08d165c37
nimabht <nimabeheshtaein99@gmail.com>
2023-02-02 11:45:11 +0330

    1-1 sol2 added!

--- 1-1/app.js
+++ 1-1/app.js
@@ -8,3 +8,20 @@ fs.appendFile("./text.txt", content, (err) => {
   }
   console.log("Text added to text.txt");
 });
+
+//-----------------------------------------------
+// if (fs.existsSync("./text.txt")) {
+//   fs.appendFile("./text.txt", content, (err) => {
+//     if (err) {
+//       console.error(err);
+//     }
+//     console.log("Text added to text.txt");
+//   });
+// } else {
+//   fs.appendFile("./text.txt", "Hello world!", (err) => {
+//     if (err) {
+//       console.error(err);
+//     }
+//     console.log("Text added to text.txt");
+//   });
+// }
